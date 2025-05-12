04da2194efbff1fd5a54cf45df805f98f37976f1
Sion Gang <siongang@gmail.com>
2025-05-12 12:17:05 -0400

    website title and icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,13 @@ import { Suspense } from "react";
 import "./globals.css"; // your global styles
 import Header from "@/components/Header";
 
+
 export const metadata = {
-  title: "My Website",
+  title: "PDF Orientation",
   description: "My cool Next.js app",
+  icons: {
+    icon: "/website_icon.png",
+  },
 };
 
 export default function RootLayout({
@@ -15,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      
       <body className="flex flex-col max-h-screen">
         <Header />
         <main className="flex flex-1 bg-stone-100 overflow-hidden">
